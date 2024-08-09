72b1453aebd0296728da60a10a46524231a97023
Yaffawijaya <yaffazka@gmail.com>
2024-08-10 03:18:00 +0700

    changing to input and computed signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+// Notes:
+// `Input` with an uppercase is decorator
+// `Input` with a lowercase is a type of function
+import { Component, computed, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -17,12 +20,16 @@ export class UserComponent {
   //   return "../assets/users/" + this.selectedUser.avatar;
   // }
 
-  @Input({ 'required': true }) avatar!: string;
-  @Input({ 'required': true }) name!: string;
+  // @Input({ 'required': true }) avatar!: string;
+  // @Input({ 'required': true }) name!: string;
 
-  get imagePath() {
-    return "../assets/users/" + this.avatar;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => "../assets/users/" + this.avatar());
+
+  // get imagePath() {
+  //   return "../assets/users/" + this.avatar;
+  // }
 
   onSelectUser() {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
