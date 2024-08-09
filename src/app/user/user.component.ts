936f77a795a018350cc7810c4ef27744cca85605
Yaffawijaya <yaffazka@gmail.com>
2024-08-10 02:47:55 +0700

    this is the user component by binding the data from the dummy users data

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => "../assets/users/" + this.selectedUser().avatar);
+
+  // get imagePath() {
+  //   return "../assets/users/" + this.selectedUser.avatar;
+  // }
+
+  @Input({ 'required': true }) avatar!: string;
+  @Input({ 'required': true }) name!: string;
+
+  get imagePath() {
+    return "../assets/users/" + this.avatar;
+  }
+
+  onSelectUser() {
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // console.log('Clicked on user', this.selectedUser);
+  }
+}
