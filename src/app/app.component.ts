36c910f465132b944cc39ce989a5c43677696e4a
Yaffawijaya <yaffazka@gmail.com>
2024-08-10 12:34:54 +0700

    make some loop and condition in app based on data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+
 import { DUMMY_USERS } from './dummy-users';
+// import { DUMMY_TASKS } from './dummy-tasks';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +14,25 @@ import { DUMMY_USERS } from './dummy-users';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    TasksComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  // tasks = DUMMY_TASKS;
+
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+  
+
 }
