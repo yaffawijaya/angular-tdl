7d0202703099b3da973387f77d9660250d8f81f5
Yaffawijaya <yaffazka@gmail.com>
2024-08-10 14:10:22 +0700

    add task model

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { type Task } from './task.model';
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+  @Output() complete = new EventEmitter<string>();
 
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
