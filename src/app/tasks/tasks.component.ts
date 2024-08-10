91416301f97b7e11e2734eb29cd79bf1c18f3e43
Yaffawijaya <yaffazka@gmail.com>
2024-08-10 15:00:36 +0700

    exploring add and delete array tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from "@angular/core";
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskData } from "./task/task.model";
 
 @Component({
   selector: "app-tasks",
   standalone: true,
   imports: [
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   templateUrl: "./tasks.component.html",
   styleUrls: ["./tasks.component.css"],
@@ -15,6 +18,7 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
 
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -55,5 +59,25 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+
+    })
+    this.isAddingTask = false;
   }
 }
