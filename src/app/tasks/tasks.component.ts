4a2bd796673c7fb9ff196910a4cd1adf7f7a8d92
Yaffawijaya <yaffazka@gmail.com>
2024-08-15 22:55:58 +0700

    migrate to NgModule style

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,13 @@
 import { Component, Input } from "@angular/core";
-import { TaskComponent } from "./task/task.component";
-import { NewTaskComponent } from "./new-task/new-task.component";
-import { NewTaskData } from "./task/task.model";
 import { TasksService } from "./tasks.service";
 
 @Component({
   selector: "app-tasks",
-  standalone: true,
-  imports: [
-    TaskComponent,
-    NewTaskComponent
-  ],
+  standalone: false,
+  // imports: [
+  //   TaskComponent,
+  //   NewTaskComponent
+  // ],
   templateUrl: "./tasks.component.html",
   styleUrls: ["./tasks.component.css"],
 })
